00601a05f79e210751c536703a23c8231bfd48c3
nicolecasares <nicolecasares@gmail.com>
2018-01-25 20:42:01 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+///////////////////////////////////
+//Advance Javascript and Functions 
+
+	//ALMOST EVERYTHING IS A OBJECT
+
+	//to types of vales PRIMATIVES and OBJECTS
+
+	//primatives are
+		//numbers, strings,booleans, undifined, null
+
+	//Objects are
+		//arrays, functions, objects, dates,
+		//wrappers for numbers, strings, booleans
+
+	//Object-Oriented Programming
+		//Objects interacting with one another throgh methods and properties
+		//Used to store data, structure applications into modules and keeping clean.
+	
+	//Constructor/Prototype
+		//aka class in other lang
+		//using this we can creat as many instances as we want
+		//acts like a blueprint 
+
+	//Inheritance 
+		//when one object is based on another object
+		//the object gets acces to another objects peroperties and methods
+
+	//Javascript is a prototype-based language 
+		//which means inheritance works by using prototypes
+		//every object has a prototype property. 
+
+	//SUMMARY from Instructor
+
+		//Every Javascript objects has a PROTOTYPE PROPERTY, which makes inheritace possible in JavaScript
+		// The prototype of an object is where we put methods and properties that we want OTHER OJECTS TO INHERIT
+		//The Constructorr's prototype is NOT the prototype of the Constructor itself, it's the prototype of ALL instances that are created through it;
+		//When a certain method (or property) is called, the search starts in the object's prototype.This continues until the method is found: prototype chain.
+		
+
+
+
+
+
+
+
+
+
+
+
